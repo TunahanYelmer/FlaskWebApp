626c5cfcb82750dc622f253d0a316306df3779d2
Tunahan Yelmer <tunahanyelmer@gmail.com>
2023-12-22 17:00:23 +0300

    html files modulerarized

--- FlaskApp/seotech_app/frontend/views.py
+++ FlaskApp/seotech_app/frontend/views.py
@@ -1,15 +1,15 @@
-from flask import  render_template , request , Blueprint
-from seotech_app.init import app , db
+from flask import render_template, Blueprint
 
-main = Blueprint('main',__name__)
+main = Blueprint("main", __name__)
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route("/", methods=["GET", "POST"])
 def home():
-    return render_template('index.html')
-@main.route('/about', methods=['GET', 'POST'])
-def about():
-    return render_template('about.html')
-@main.route('/service',methods=['GET','POST'])
-def service():
-    return render_template('service.html')
+    return render_template("home.html", home="home", show_slider=True)
 
+@main.route("/about", methods=["GET", "POST"])
+def about():
+    return render_template("about.html", about="about" ,show_slider=True) 
+
+@main.route("/service", methods=["GET", "POST"])
+def service():
+    return render_template("service.html", service="service" ,show_slider=False) 
--- FlaskApp/seotech_app/init.py
+++ FlaskApp/seotech_app/init.py
@@ -1,17 +1,16 @@
 from flask import Flask
 from flask_pymongo import PyMongo
-import os 
 
-app =Flask(__name__)
-app.config['MONGO_URI']='mongodb+srv://tunahanyelmer:<TunaYel.41>@cluster0.zwojfzw.mongodb.net/?retryWrites=true&w=majority'
+app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb+srv://tunahanyelmer:<TunaYel.41>@cluster0.zwojfzw.mongodb.net/?retryWrites=true&w=majority'
 db = PyMongo(app)
+
+def check_connection():
+    return db.cx.server_info()
 
 from seotech_app.frontend.views import main
 app.register_blueprint(main)
-def check_connection():
-    return db.cx.server_info()  # This will throw an exception if it cannot connect to the MongoDB server
 
 if __name__ == "__main__":
     print(check_connection())  # Print server info to check connection
     app.run(debug=True)
-
